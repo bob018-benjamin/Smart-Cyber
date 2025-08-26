69877a0ea2464038ed8ef45267985780b3c5f53b
bob018-benjamin <128193226+bob018-benjamin@users.noreply.github.com>
2025-08-26 13:30:18 +0300

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,5 +1,8 @@
 // js/supabaseClient.js
-const SUPABASE_URL = 'https://xbkloxgdqnxuubdnjwzk.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...'; // use full anon key here
 
+// Replace with your actual project URL and anon key
+const SUPABASE_URL = 'https://xbkloxgdqnxuubdnjwzk.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhia2xveGdkcW54dXViZG5qd3prIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYxMzU0NDksImV4cCI6MjA3MTcxMTQ0OX0.6n8c6ZzFTOCAXM-RN8LrkpfxHil2nTV35ArEGgrs_9w';
+
+// Create a Supabase client
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
